362ec237a48ce05635c44c4cc1cab15352baae6c
1205762664@qq.com <1205762664@qq.com>
2024-06-08 07:08:46 +0800

    first commit

---
+++ yesorno-server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/generate-answer', (req, res) => {
+  const answer = Math.random() < 0.5 ? 'yes' : 'no';
+  res.json({ answer });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
