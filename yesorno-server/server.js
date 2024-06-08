d7b9d299616f009d8779fffad03c5793629a3109
1205762664@qq.com <1205762664@qq.com>
2024-06-08 22:38:37 +0800

    3344

--- yesorno-server/server.js
+++ yesorno-server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -7,11 +8,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// 设置静态文件目录，这里假设index.html位于根目录下
+
+app.use(express.static(path.join(__dirname, '')));
+
+// 处理POST请求到/generate-answer
 app.post('/generate-answer', (req, res) => {
   const answer = Math.random() < 0.5 ? 'yes' : 'no';
   res.json({ answer });
 });
 
+// 如果上面的静态文件服务没有处理请求，则默认返回index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-const PORT = process.env.VCLOUD_PORT || process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
