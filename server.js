4e587a9d5516901349d4e97591422ed4ef9d86e0
1205762664@qq.com <1205762664@qq.com>
2024-06-10 13:51:01 +0800

    061013

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // 设置静态文件目录，这里假设index.html位于根目录下
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 处理POST请求到/generate-answer
 app.post('/generate-answer', (req, res) => {
@@ -22,7 +22,7 @@ app.post('/generate-answer', (req, res) => {
 
 // 处理所有GET请求以返回index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
